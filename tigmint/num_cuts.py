323aa949cb7705a21813c02248f7b8a028a44dae
janetxinli <janetxinli@gmail.com>
2020-11-10 12:18:39 -0800

    clean up

--- tigmint/num_cuts.py
+++ tigmint/num_cuts.py
@@ -1,29 +1,27 @@
-#!/usr/bin/env python3
-"""
-Counts the number of cuts made to contigs in a bed file.
-Usage: python3 /projects/btl/scratch/janli/scripts/num_cuts.py <bedfile>
-Author: Janet Li @janetxinli
-"""
+#!/usr/bin/env python
+"""Count the number of cuts made to contigs in a tigmint breaktig bed file."""
 
 import sys
 
-class LastCutContig:
+class Contig:
+	""""""
 	def __init__(self):
 		self.cuts = 0	
 	def addCut(self):
 		self.cuts += 1
 
+
 def count_breaktigs(fh):
 	"""Given the file handle of a bed file, count the breaktigs in the file."""
 	total_cuts = 0
-	cur_breaktig = LastCutContig()
+	cur_breaktig = Contig()
 	for line in fh:
 		cur_contig = line.strip().split(sep="\t")
 		contig_name = cur_contig[3].split("-")
 		if len(contig_name) > 1:
 			if contig_name[1] == "1":  # First breaktig of a contig
 				total_cuts += cur_breaktig.cuts
-				cur_breaktig = LastCutContig()  # Start a new breaktig
+				cur_breaktig = Contig()  # Start a new breaktig
 			else:
 				cur_breaktig.addCut()
 
@@ -31,16 +29,18 @@ def count_breaktigs(fh):
 	total_cuts += cur_breaktig.cuts
 	return total_cuts
 
+
 def main():
 	if len(sys.argv) != 2 or not sys.argv[1].endswith(".bed"):
-		print("num_cuts: error: must provide bed file as an argument.")
-		print("usage: %s <breaktigs bed file>" % sys.argv[0])
+		print("num_cuts.py: error: must provide bed file as an argument", file=sys.stderr, flush=True)
+		print("usage: %s <breaktigs bed file>" % sys.argv[0], file=sys.stderr, flush=True)
 		sys.exit(1)
 	
 	bed = sys.argv[1]
 	with open(bed, "r") as contigs:
 		cuts = count_breaktigs(contigs)	
-	print(cuts)
+	print(cuts, file=sys.stdout)
+
 
 if __name__ == "__main__":
 	main()
