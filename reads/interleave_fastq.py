5730fc4a5fc4c8cd4f9965b9e68c4f112aa9c931
janetxinli <janetxinli@gmail.com>
2020-11-10 15:17:45 -0800

    revert changes

--- reads/interleave_fastq.py
+++ reads/interleave_fastq.py
@@ -8,12 +8,12 @@ import glob
 
 def interleave_reads(read1, read2, outfile):
     """Combined reads from r1 and r2 in interleaved format and print to outfile."""
-    with gzip.open(read1, "rt") as r1, gzip.open(read2, "rt") as r2:
-        for i, line in enumerate(r1):
-            outfile.write(line)
-            if i % 4 == 3:
-                for _ in range(4):
-                    outfile.write(r2.readline())
+    with gzip.open(read1, "rt") as r1, gzip.open(read2, "rt") as r2, open(outfile, "w+") as out:
+        while True:
+            for _ in range(4):
+                out.write(r1.readline())
+            for _ in range(4):
+                out.write(r2.readline())
 
 
 def get_args():
