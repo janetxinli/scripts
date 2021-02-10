9f0b0dc7faa09f37585ad02619a696309cb599de
janetxinli <janetxinli@gmail.com>
2021-02-10 10:52:09 -0800

    option to print without name

--- gff/gene_length_histogram.py
+++ gff/gene_length_histogram.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-# Print a histogram of gene lengths
+# Print a histogram of gene lengths to stdout
 # Expected format:
 # scaf926 .       contig  1       6497    .       .       .       ID=scaf926;Name=scaf926
 
 import sys
+import argparse
 import numpy as np
+from utils import print_histogram
 
 def get_gene_lengths(gff):
     """Returns a list of gene lengths from gff file."""
     gene_lengths = []
     with open(gff, "r") as fh:
         for line in fh:
-            if line[0] == ">":
+            if line[0] == ">":  # Skip fasta sequences at end of file
                 break
             if line[0] != "#":
                 #print(line)
@@ -23,23 +25,40 @@ def get_gene_lengths(gff):
     return gene_lengths
 
 
-def print_histogram(gene_lengths, name):
-    """Prints histogram of gene lengths with bin size=100."""
+def print_named_histogram(gene_lengths, binwidth, name):
+    """Prints histogram of gene lengths with a given name."""
     max_length = max(gene_lengths)
     hist_max = (100 - (max_length % 100)) + max_length
-    counts, bins = np.histogram(gene_lengths, bins=hist_max//100, range=(0, hist_max))
+    counts, bins = np.histogram(gene_lengths, bins=hist_max//binwidth, range=(0, hist_max))
     for i, b in enumerate(bins[1:]):
         print(int(b), counts[i], name, sep="\t", flush=True)
 
 
+def parse_args():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description="Extract and print gene lengths as a histogram")
+    parser.add_argument("-g", "--gff",
+                        type=str,
+                        default="/dev/stdin",
+                        help="Input GFF file")
+    parser.add_argument("-n", "--name",
+                        type=str,
+                        default=None,
+                        help="Name of gff file to print to histogram")
+    parser.add_argument("-b", "--binwidth",
+                        type=int,
+                        default=100,
+                        help="Bin width for histogram")
+    return parser.parse_args()
+
+
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: {} <gff file> <name>".format(sys.argv[0]), file=sys.stderr)
-        sys.exit(1)
-    gff = sys.argv[1]
-    name = sys.argv[2]
-    gene_lengths = get_gene_lengths(gff)
-    print_histogram(gene_lengths, name)
+    args = parse_args()
+    gene_lengths = get_gene_lengths(args.gff)
+    if args.name is None:
+        print_histogram(gene_lengths, args.binwidth)
+    else:
+        print_named_histogram(gene_lengths, args.binwidth, args.name)
 
 
 if __name__ == "__main__":
