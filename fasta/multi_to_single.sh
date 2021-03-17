4a0b957a1501563a34f46e9524fe7c1d1279c3c4
Janet Li <janetxinli@gmail.com>
2021-03-17 12:33:08 -0700

    allow gzipped files

--- fasta/multi_to_single.sh
+++ fasta/multi_to_single.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # Convert a multi-line fasta file to single-line
 
-if [[ $# -ne 1 ]]; then
+if [[ $# -lt 1 ]]; then
 	echo "Usage: $(basename $0) <multiline fasta>"
 	exit 1
 fi
 
-cat ${1} | awk '/^>/{if(N>0) printf("\n"); ++N; printf("%s\n",$0);next;} {printf("%s",$0);}END{printf("\n");}'
+if [[ $1 =~ .gz$ ]]; then
+	method="gunzip -c"
+else
+	method="cat"
+fi
+
+${method} ${1} | awk '/^>/{if(N>0) printf("\n"); ++N; printf("%s\n",$0);next;} {printf("%s",$0);}END{printf("\n");}'
