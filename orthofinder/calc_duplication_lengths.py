ff41e9363409e8b7ffe1caf6fc54ff0452f2f492
janetxinli <janetxinli@gmail.com>
2021-05-06 13:53:01 -0700

    calculate total length of duplicated genes

---
+++ orthofinder/calc_duplication_lengths.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+"""
+Calculate total length of duplicated orthogroups on Terminal nodes (events that
+occurred within a species).
+"""
+
+import sys
+import argparse
+import re
+
+def read_dups(infile="-"):
+    """Read duplicated genes names."""
+    genes = []
+    if infile is None:
+        if sys.stdin.isatty():
+            print("calc_duplication_lengths.py: error: gene names must "
+                  "be piped from stdin or passed as an argument")
+            sys.exit(1)
+        fh = sys.stdin
+    else:
+        fh = open(infile, "r")
+
+    for line in fh:
+        genes.append(line.strip())
+    
+    fh.close()
+    return genes
+
+def get_gene_lengths(gff):
+    """Load all gene lengths from GFF file."""
+    gene_re = "ID=(JB-[0-9]{2}_\d+);.+"
+    gene_lengths = {}  # maker id -> gene length
+    with open(gff, "r") as fh:
+        for line in fh:
+            if not line.startswith("#"):
+                line = line.strip().split("\t")
+                if line[2] == "gene":
+                    gene = re.search(gene_re, line[8])[1]
+                    length = int(line[4]) - int(line[3])
+                    gene_lengths[gene] = length
+    
+    return gene_lengths
+
+def parse_args():
+    """Parse the command line arguments."""
+    parser = argparse.ArgumentParser(description="Calculation duplication length on "
+                                     "a terminal OrthoFinder species tree node")
+    parser.add_argument("node",
+                        type=str,
+                        help="Species tree node")
+    parser.add_argument("gff",
+                        type=str,
+                        help="Annotation GFF file corresponding to species "
+                             "tree node")
+    parser.add_argument("dups",
+                        type=str,
+                        nargs="?",
+                        default=None,
+                        help="Duplicated genes (line-separated) [stdin]")                             
+    
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    duplicated_genes = read_dups(args.dups)
+    lengths = get_gene_lengths(args.gff)
+    
+    # Calculate total dup length
+    total_length = 0
+    for gene in duplicated_genes:
+        total_length += lengths[gene]
+    
+    print(total_length)
+
+if __name__ == "__main__":
+    main()
