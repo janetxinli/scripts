66e4156d02a449b7d4f4424cbb9e12bea4ce9ab3
Janet Li <janetxinli@gmail.com>
2021-05-28 09:16:14 -0700

    remove unnecessary import

--- orthofinder/__init__.py
+++ orthofinder/__init__.py
@@ -1,3 +1,2 @@
 from .summarize_orthogroups import is_core, is_single_core, is_accessory, is_singleton, get_orthogroup_genes
 from .get_orthogroups import load_orthogroups
-from .orthogroup_gmt import load_gff_go
