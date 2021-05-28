625e09d59357699b866b2c1bbfc3f06a702d34d2
janetxinli <janetxinli@gmail.com>
2021-05-28 09:15:16 -0700

    get pfam description

--- orthofinder/__init__.py
+++ orthofinder/__init__.py
@@ -1,3 +1,4 @@
 from .summarize_orthogroups import is_core, is_single_core, is_accessory, is_singleton, get_orthogroup_genes
 from .get_orthogroups import load_orthogroups
 from .orthogroup_gmt import load_gff_go
+from .get_group_specific import get_pfam_desc
