10872989dd6ad3ea0b4d9a1d0e02ededd4c00937
Janet Li <janetxinli@gmail.com>
2021-07-20 11:03:17 -0700

    get functional info from gff file

--- gff/__init__.py
+++ gff/__init__.py
@@ -1,3 +1,3 @@
 from .constants import *
-from .mrna_functional_info import mrna_functional_info
+from .functional_info import functional_info
 from .mrna_note import mrna_note
--- gff/functional_info.py
+++ gff/functional_info.py
@@ -4,22 +4,22 @@
 import re
 from gff import ID_RE, GO_RE, PFAM_RE
 
-def mrna_functional_info(gff):
+def functional_info(gff, feature="mRNA"):
     """
     Get functional info (GO terms and Pfam domains) from
     a MAKER gff file.
     """
-    func = {}  # mrna_id -> ([go terms], [pfam domains])
+    func = {}  # feature_id -> ([go terms], [pfam domains])
 
     with open(gff, "r") as fh:
         for line in fh:
             if not line.startswith("#"):
                 line = line.strip().split("\t")
-                if line[2] == "mRNA":
+                if line[2] == feature:
                     info = line[8]
-                    mrna_id = re.search(ID_RE, info)[1]
+                    id = re.search(ID_RE, info)[1]
                     go_terms = re.findall(GO_RE, info)
                     pfam_doms = re.findall(PFAM_RE, info)
-                    func[mrna_id] = (go_terms, pfam_doms)
+                    func[id] = (go_terms, pfam_doms)
     
     return func
